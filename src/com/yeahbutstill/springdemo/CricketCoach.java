f5be30eef20acea3f4bb5f14aa100602b00f8bcb
yeahbutstill <mayadani28@gmail.com>
2022-05-27 09:35:13 +0700

    injection literal values

--- src/com/yeahbutstill/springdemo/CricketCoach.java
+++ src/com/yeahbutstill/springdemo/CricketCoach.java
@@ -1,9 +1,10 @@
 package com.yeahbutstill.springdemo;
 
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // add new fields for email and team
     private String emailAddress;
     private String team;
 
@@ -12,17 +13,31 @@ public class CricketCoach implements Coach{
         System.out.println("CricketCoach: inside no-arg constructor");
     }
 
+    public FortuneService getFortuneService() {
+        return fortuneService;
+    }
+
     // our setter method
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
     }
 
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
     public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
         this.emailAddress = emailAddress;
     }
 
+    public String getTeam() {
+        return team;
+    }
+
     public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
         this.team = team;
     }
 
