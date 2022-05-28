3d0dfc1e1bb28a803412f9b4430a37a7bbee1607
yeahbutstill <mayadani28@gmail.com>
2022-05-28 09:45:59 +0700

    constructor injection

--- spring-demo-annotations/src/com/yeahbutstill/springdemos/TennisCoach.java
+++ spring-demo-annotations/src/com/yeahbutstill/springdemos/TennisCoach.java
@@ -1,11 +1,22 @@
 package com.yeahbutstill.springdemos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Spring will scan for component that implements
+ */
 @Component
 public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
+
+    // configure dependency injection with @Autowired annotation
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        System.out.println("fortuneService " + fortuneService);
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
