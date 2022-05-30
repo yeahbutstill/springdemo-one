cccc113460eb4aab3b9018c3fa4ecccb090ea58d
yeahbutstill <mayadani28@gmail.com>
2022-05-30 18:15:10 +0700

    method injection

--- spring-demo-annotations/src/com/yeahbutstill/springdemos/TennisCoach.java
+++ spring-demo-annotations/src/com/yeahbutstill/springdemos/TennisCoach.java
@@ -11,23 +11,34 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // define a default constructor
     public TennisCoach() {
+        System.out.println(">> inside default constructor");
     }
 
+    // constructor injection method
     // configure dependency injection with @Autowired annotation
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        System.out.println("fortuneService " + fortuneService);
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        System.out.println("fortuneService " + fortuneService);
+//        this.fortuneService = fortuneService;
+//    }
 
     public FortuneService getFortuneService() {
         return fortuneService;
     }
 
+    // define a setter injection method
     @Autowired
     public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("fortuneService " + fortuneService);
+        System.out.println(">> TennisCoach: inside setFortuneService() method " + fortuneService);
+        this.fortuneService = fortuneService;
+    }
+
+    // method injection
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method " + fortuneService);
         this.fortuneService = fortuneService;
     }
 
